697eff10f5bc331ebf7dd29919ff47e334b2dccf
Liangchen <1140148711@qq.com>
2022-10-18 15:30:30 +0800

    third commit

--- src/main/java/com/cauc/git/GitTest.java
+++ src/main/java/com/cauc/git/GitTest.java
@@ -3,5 +3,6 @@ package com.cauc.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello cauc");
+        System.out.println("hello cauc2");
     }
 }
